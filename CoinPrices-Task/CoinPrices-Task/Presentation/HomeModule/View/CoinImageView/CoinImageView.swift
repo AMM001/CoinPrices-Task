47808397da9880fe93199cdf07eb773a0fbbe8e6
abdelmenim mohamed <abdelmenim.mohamed97@gmail.com>
2023-05-20 15:19:58 +0300

    feat: add kingfisher to download image

--- CoinPrices-Task/CoinPrices-Task/Presentation/HomeModule/View/CoinImageView/CoinImageView.swift
+++ CoinPrices-Task/CoinPrices-Task/Presentation/HomeModule/View/CoinImageView/CoinImageView.swift
@@ -7,27 +7,21 @@
 
 import Foundation
 import SwiftUI
+import Kingfisher
 
 struct CoinImageView: View {
     
-    @StateObject var vm: CoinImageViewModel
+    private let coin: CoinModel
     
     init(coin: CoinModel) {
-        _vm = StateObject(wrappedValue: CoinImageViewModel(coin: coin))
+        self.coin = coin
     }
     
     var body: some View {
         ZStack {
-            if let image = vm.image {
-                Image(uiImage: image)
-                    .resizable()
-                    .scaledToFit()
-            } else if vm.isLoading {
-                ProgressView()
-            } else {
-                Image(systemName: "questionmark")
-                    .foregroundColor(Color.theme.secondaryText)
-            }
+            let url = URL(string: coin.image)
+            KFImage(url)
+                .resizable()
         }
     }
 }
